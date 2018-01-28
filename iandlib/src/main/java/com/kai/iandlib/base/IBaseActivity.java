d2e6ac0862090c6fab7de8b36d882718578e36c1
fengfenkai <fengfenkai@icarbonx.com>
2018-01-28 23:22:15 +0800

    init  version

---
+++ iandlib/src/main/java/com/kai/iandlib/base/IBaseActivity.java
@@ -0,0 +1,22 @@
+package com.kai.iandlib.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Author:  Kevin Feng
+ * Email:   597415099@qq.com
+ * Date:    2018-01-28 0:10
+ * Description:
+ */
+public class IBaseActivity extends FragmentActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
